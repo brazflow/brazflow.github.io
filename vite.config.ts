e45487913ab64700d54396b11f592f86282f17d4
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2026-01-05 00:28:20 -0400

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "https://brazflow.github.io",
   server: {
     allowedHosts: ["heros-server.tail2e14dd.ts.net"],
     proxy: {
