3843841b58861818b031d33a5ba1e7eae5db0ac4
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2026-01-05 14:37:47 -0400

    Logo should be a link to home

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 
 export default function BrandLogo(){
   return (
-    <div className="brand-logo flex gap-3 items-center bg-gradient-to-r from-[#5372F0]/10 to-[#215BBF]/5 p-2 rounded-md border border-[#5372F0]/20" role="img" aria-label="BrazFlow logo">
+    <Link to="/" className="brand-logo flex gap-3 items-center bg-gradient-to-r from-[#5372F0]/10 to-[#215BBF]/5 p-2 rounded-md border border-[#5372F0]/20" role="img" aria-label="BrazFlow logo">
       <div className="brand-icon text-3xl drop-shadow-md transform -translate-y-1">🌊</div>
       <div className="brand-text">
         <div className="brand-title font-extrabold">BrazFlow</div>
         <div className="brand-sub text-sm text-white/60">Water Availability</div>
       </div>
-    </div>
+    </Link>
   )
 }
